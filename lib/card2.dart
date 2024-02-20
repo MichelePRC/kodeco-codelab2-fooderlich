f3db3e6690b6a668b1ad7467c7c16f0e6adea478
MichelePRC <m.procopio91@gmail.com>
2024-02-20 01:00:11 +0100

    card2 and circle_image added

---
+++ lib/card2.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Card2 extends StatelessWidget {
+  const Card2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: BoxConstraints.expand(width: 350, height: 450),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag5.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Column(
+          children: const [
+            //TODO: aggiungere 2 widget text
+          ],
+        ),
+      ),
+    );
+  }
+}
